133e105ca893db58bf2f3cfe4149956cd037f7c9
Abhilash Ghogale <abhilash.ghogale@kpit.com>
2020-04-24 16:41:37 +0530

    iOS project created

---
+++ iTableDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTableDemo
+//
+//  Created by Abhilash Ghogale on 24/04/2020.
+//  Copyright © 2020 abhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
